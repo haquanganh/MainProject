883fc3ec478a7f8e3043dd1cffe0550c1231b807
Astro <haquanganhdn@gmail.com>
2016-04-11 09:04:00 +0700

    Fix

--- app/Http/Controllers/Admin/FeedbackController.php
+++ app/Http/Controllers/Admin/FeedbackController.php
@@ -9,11 +9,11 @@ use App\Http\Requests;
 use App\Feedback_History as Feedback_History;
 class FeedbackController extends Controller{
 	public function getviewoldFeedback($time,$id){
-        $histories= Feedback_History::orderBy('H_DateCreate','DESC')->where('idFeedback','=',$id)->get();
-        return view('admin.feedback_old', compact('histories','time'));
+        $feedback_olds = Feedback_History::orderBy('H_DateCreate','ASC')->where('idFeedback','=',$id)->get();
+        $histories = array();
+        for($i = count($feedback_olds) - $time -1  ; $i >= 0 ; $i--){
+            array_push($histories, $feedback_olds[$i]);
+        }
+        return view('admin.feedback_old', compact('histories'));
     }
 }
-
-
-
-
